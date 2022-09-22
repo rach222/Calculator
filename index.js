cd279b3bdc86cb6b8cd4e52e9ff5f65319ce630a
rache <rachellmarshall1998@gmail.com>
2022-09-22 12:15:13 +0100

    Added a simple multiplier

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+const readline = require('readline-sync');
+
 console.log('Welcome to the calculator!');
+
+console.log('Please enter your first number:');
+const var1 = parseFloat(readline.prompt());
+
+console.log('Please enter your second number:');
+const var2 = parseFloat(readline.prompt());
+
+console.log('The product of ' + var1 +' and ' + var2 + ' is ' + var1*var2);
