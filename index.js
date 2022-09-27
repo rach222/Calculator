cf74566399f38f899fd4644e1e1161dc365ae5c9
rache <rachellmarshall1998@gmail.com>
2022-09-27 12:43:30 +0100

    Added vowel counter

--- index.js
+++ index.js
@@ -11,22 +11,30 @@ function printWelcomeMessage() {
     console.log('==========================')
 }
 
+function printGoodbyeMessage() {
+    console.log("Thank you for using the calculator.")
+}
+
 function requestNumberWithPrompt(prompt) {
-    var number;
     console.log(prompt);
-    number = readline.prompt()
-    if (isNaN(number)) {
-        console.log("Not a valid number, please try again.");
+    var number = readline.prompt();
+    if (isNaN(+number)) {
+        console.log(`${number} is not a valid number, please try again.`);
         return requestNumberWithPrompt(prompt);
     }
-    return number;
+    return +number;
+}
+
+function requestStringWithPrompt(prompt) {
+    console.log(prompt);
+    var string = readline.prompt();
+    return string;
 }
 
 function requestOperator(){
-    console.log('Please enter an operator:');
-    const operator = readline.prompt();
+    const operator = requestStringWithPrompt('Please enter an operator:');
     
-    if (!(operator == '+' || operator == '-' || operator == '*' || operator == '/')) {
+    if (!(operator in symbolToWordDict)) {
     console.log('Operator not accepted, try again.');
     return requestOperator();
     }
@@ -34,71 +42,103 @@ function requestOperator(){
 }
 
 function requestQuantity(operator) {
-    return requestNumberWithPrompt('How many numbers do you want to ' + symbolToWordDict[operator] + "?:");
+    return requestNumberWithPrompt(`How many numbers do you want to ${symbolToWordDict[operator]}?:`);
 }
 
 function formArray(operator) {
     const quantity = requestQuantity(operator);
     var numbers = [];
     for (let step = 0; step < quantity; step++) {
-        numbers.push(requestNumberWithPrompt("Please enter number " + (step+1) + ":"));
+        numbers.push(requestNumberWithPrompt(`Please enter number ${step+1}:`));
     }
     return numbers;
 }
 
 function calculateAnswer(operator, numbers) {
-    const quantity = numbers.length;
     var answer = numbers[0];
+    for (let step = 1; step < numbers.length; step++) {
     switch (operator) {
         case '+':
-        for (let step = 1; step < quantity; step++) {
             answer += +numbers[step];
-        }
+        
         break;
 
         case '*':
-        for (let step = 1; step < quantity; step++) {
             answer *= +numbers[step];
-        }
+        
         break;
 
         case '-':
-        for (let step = 1; step < quantity; step++) {
             answer -= +numbers[step];
-        }
+        
         break;
 
         case '/':
-        for (let step = 1; step < quantity; step++) {
             answer /= +numbers[step];
-        }
+        
         break;
+    }
     }
     return answer;
 }
 
-function performOneOperation() {
-    
+function performOneArithmeticOperation() {
     const operator = requestOperator();
     const numbers = formArray(operator);
     const answer = calculateAnswer(operator, numbers);    
-    console.log('The answer is ' + answer);
+    console.log(`The answer is ${answer}.`);
 }
 
-function runCalculator() {
-    var continueCalculator = true;
-    while (continueCalculator) {
-        performOneOperation();
-        console.log("Do you want to perform another calculation? (y/n)");
-        let response = readline.prompt();
-        if (!(response == "y" || response == "Y")) {
-            continueCalculator = false;
-        }
+function getCalculationMode() {
+    const mode = requestNumberWithPrompt("Which mode do you want to use? \n1)Arithmetic\n2)Vowel Counting");
+    if (mode === 1 || mode ===2) {
+        return mode;
+    }
+    else {
+        console.log('Number not accepted, enter 1 or 2.')
+        getCalculationMode();
     }
 }
 
-function printGoodbyeMessage() {
-    console.log("Thank you for using the calculator.")
+function askContinue() {
+    const response = requestStringWithPrompt("Do you want to perform another calculation? (y/n)");
+    if (!(response == "y" || response == "Y")) {
+        return false;
+    }
+    else {
+        return true;
+    }
+}
+
+function performOneVowelCount() {
+    const string = requestStringWithPrompt("Please enter string to be counted:");
+    const count = {'A':0, 'E':0, 'I':0, 'O':0, 'U':0};
+    for (var idx=0; idx<string.length; idx++) {
+        if (string[idx].toUpperCase() in count) {
+            count[string[idx].toUpperCase()]++;
+        }
+    }
+    console.log(count);
+}
+
+function runCalculator() {
+    
+    var continueCalculator = true;
+    
+    while (continueCalculator) {
+        const calculationMode = getCalculationMode();
+        switch (calculationMode) {
+            case 1:
+                performOneArithmeticOperation();
+                break;
+            case 2:
+                performOneVowelCount();
+                break;
+        }
+
+        continueCalculator = askContinue();
+
+    }
 }
 
 printWelcomeMessage();
